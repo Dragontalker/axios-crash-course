aa8387ffc0ecebba2566e74a4b959b24f86878b9
Richard <richard.yang.tong@gmail.com>
2021-03-18 10:28:50 -0400

    Improved the indentation of showOuput function and tested on get request.

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const getTodos = async () => {
     console.log('GET Request');
     // Using Axios
     const result = await axios({ method: 'get', url: my_url });
-    console.log(result);
+    showOutput(result);
 };
 
 // POST REQUEST
@@ -53,36 +53,36 @@ console.log('Cancel Token');
 // AXIOS INSTANCES
 
 // Show output in browser
-function showOutput(res) {
-document.getElementById('res').innerHTML = `
-<div class="card card-body mb-4">
-    <h5>Status: ${res.status}</h5>
-</div>
-<div class="card mt-3">
-    <div class="card-header">
-    Headers
-    </div>
-    <div class="card-body">
-    <pre>${JSON.stringify(res.headers, null, 2)}</pre>
-    </div>
-</div>
-<div class="card mt-3">
-    <div class="card-header">
-    Data
-    </div>
-    <div class="card-body">
-    <pre>${JSON.stringify(res.data, null, 2)}</pre>
-    </div>
-</div>
-<div class="card mt-3">
-    <div class="card-header">
-    Config
-    </div>
-    <div class="card-body">
-    <pre>${JSON.stringify(res.config, null, 2)}</pre>
-    </div>
-</div>
-`;
+const showOutput = (res) => {
+    document.getElementById('res').innerHTML = `
+        <div class="card card-body mb-4">
+            <h5>Status: ${res.status}</h5>
+        </div>
+        <div class="card mt-3">
+            <div class="card-header">
+                Headers
+            </div>
+            <div class="card-body">
+                <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+            </div>
+        </div>
+            <div class="card mt-3">
+            <div class="card-header">
+                Data
+            </div>
+            <div class="card-body">
+                <pre>${JSON.stringify(res.data, null, 2)}</pre>
+            </div>
+        </div>
+        <div class="card mt-3">
+            <div class="card-header">
+                Config
+            </div>
+            <div class="card-body">
+                <pre>${JSON.stringify(res.config, null, 2)}</pre>
+            </div>
+        </div>
+    `;
 }
 
 // Event listeners
